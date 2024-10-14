6aac09b2baeb70f18fa13210fa2f748937e0a88c
Jrgil.20 <Jrgil.20@est.ucab.edu.ve>
2024-10-14 09:38:08 -0400

    Refactor list.cpp to implement a LinkedList class

--- list/list.cpp
+++ list/list.cpp
@@ -1,50 +1,70 @@
 #include <iostream>
 
+
+/*creamos la estructura*/
 struct Node {
     int data;
     Node* next;
 };
-s
+
+/*creamos la clase LinkedList*/
 class LinkedList {
 public:
+    /*constructor de la clase*/
+    Node* head;
+
     LinkedList() : head(nullptr) {}
 
-    void insert(int value) {
-        Node* newNode = new Node();
-        newNode->data = value;
-        newNode->next = head;
-        head = newNode;
-    }
+    /*metodo para agregar un nodo*/
+    void addNode(int value) {
+        Node* newNode = new Node(); //creamos un nuevo nodo
+        newNode->data = value;   //asignamos el valor al nodo
+        newNode->next = nullptr; //asignamos el siguiente nodo como nulo
 
-    void display() const {
-        Node* current = head;
-        while (current != nullptr) {
-            std::cout << current->data << " -> ";
-            current = current->next;
-        }
-        std::cout << "nullptr" << std::endl;
-    }
-
-    ~LinkedList() {
-        Node* current = head;
-        while (current != nullptr) {
-            Node* next = current->next;
-            delete current;
-            current = next;
+        if (head == nullptr) { //si la lista esta vacia el nuevo nodo es cabeza
+            head = newNode; 
+        } else {    //si no recorremos la lista hasta el final y agregamos el nuevo nodo
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode; //agregamos el nuevo nodo al final
         }
     }
 
-private:
-    Node* head;
+    void addNodeinHead(int value) {
+        Node* newNode = new Node(); //creamos un nuevo nodo
+        newNode->data = value;   //asignamos el valor al nodo
+        newNode->next = head; //asignamos el siguiente nodo como nulo
+
+        head = newNode; //si la lista esta vacia el nuevo nodo es cabeza
+    }
+
+
+    /*metodo para imprimir la lista*/
+    void printList() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
+/*funcion principal: example*/
 int main() {
-    LinkedList list;
-    list.insert(10);
-    list.insert(20);
-    list.insert(30);
+    LinkedList list1;
+    list1.addNode(1);
+    list1.addNode(2);
+    list1.addNodeinHead(0);
+    list1.printList(); // Output: 0 1 2
 
-    list.display();
+    LinkedList list2;
+    list2.addNode(10);
+    list2.addNode(20);
+    list2.addNodeinHead(5);
+    list2.printList(); // Output: 5 10 20
 
     return 0;
 }
